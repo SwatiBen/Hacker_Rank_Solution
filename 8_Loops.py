54da9555dd517f43f428cc64384d5eb3d7f4e702
Swati Beniwal <101699580+SwatiBen@users.noreply.github.com>
2022-06-22 23:31:26 +0530

    Add files via upload

---
+++ 8_Loops.py
@@ -0,0 +1,9 @@
+# Task
+# The provided code stub reads and integer, n, from STDIN. For all non-negative integers i<n , print i^2.
+
+if __name__ == '__main__':
+    n = int(raw_input())
+   
+    for i in range(n):
+        result = i**2
+        print(result)
