50016181b526be996bde4033e5f02461d2167042
Swati Beniwal <101699580+SwatiBen@users.noreply.github.com>
2022-06-22 23:32:06 +0530

    Add files via upload

---
+++ 10_Find the runner up score.py
@@ -0,0 +1,17 @@
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. 
+# You are given n scores. Store them in a list and find the score of the runner-up.
+
+#Input Format
+
+#The first line contains n. The second line contains an array A[]  of n integers each separated by a space.
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    arr = map(int, raw_input().split())
+
+    a = max(arr)
+    c = arr.count(a)
+    for i in range(c):
+        arr.remove(a)
+        
+    print(max(arr))  
